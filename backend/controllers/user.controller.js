a255e3915dd059a952ddd71d2446251f4ba8159f
AMAN RAJ <raj117557@gmail.com>
2024-12-19 12:09:48 +0530

    user login api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -26,3 +26,20 @@ module.exports.registerUser = async (req, res, next) => {
     .status(201)
     .json({ message: "User registered successfully", user, token });
 };
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password " });
+  }
+  const isPasswordValid = await user.comparepassword(password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+  const token = user.generateAuthToken();
+  res.status(200).json({ message: "User logged in successfully", user, token });
+};
